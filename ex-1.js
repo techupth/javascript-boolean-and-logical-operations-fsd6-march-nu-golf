4308003eb1242550d8c0f4cd6be8662061ef2558
Piyachat Jongtong <wearingandtearing5221@gmail.com>
2024-05-09 15:13:33 +0700

    exercise-Teal-Golf

--- ex-1.js
+++ ex-1.js
@@ -5,4 +5,13 @@ let isAllow;
 
 // Start coding here
 
+const james = {
+  age: 18,
+  hasCriminal: false,
+};
+
+isOver18 = james.age > 18 ? true : false;
+hasCriminalBlacklist = james.hasCriminal;
+isAllow = isOver18 && hasCriminalBlacklist;
+
 console.log(isAllow);
