80808022bb728d9ca59f826e3af79f4fa630508e
Kunakorn <kunakorn.ph98@gmail.com>
2024-05-09 15:31:54 +0700

    Assignment#4 March

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,8 @@
-let isOver18;
-let hasCriminalBlacklist;
-
-let isAllow;
+let age = 18
+let criminalBlacklist = false
+let isOver18 = age > 18
+let hasCriminalBlacklist = criminalBlacklist == false
+let isAllow = criminalBlacklist&&hasCriminalBlacklist
 
 // Start coding here
-
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,16 @@
 // Exercise #2: Promotion Conditions
-
+let johnLastMonthPay = 4001
+let johnLastBuyDate = 'Friday'
+let johnBoughtProductFromITCategory = false
+let johnAttendedDiscountEventTime = 2
+let johnMember = 'Gold'
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = johnLastMonthPay > 4000;
+let isWeekday = johnLastBuyDate == 'Friday'||johnLastBuyDate == 'Monday';
+let hasBoughtProductFromITCategory = johnBoughtProductFromITCategory == true;
+let hasAttendedDiscountEvent = johnAttendedDiscountEventTime == 0;
+let isPlatinum = johnMember == 'Platinum';
 
-let hasPromotion;
+let hasPromotion = lastMonthPaidMoreThan4000 && isWeekday && hasBoughtProductFromITCategory &&hasAttendedDiscountEvent && isPlatinum;
 
 console.log(hasPromotion);
